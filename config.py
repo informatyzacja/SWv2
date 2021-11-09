6dfbfa468c52b6bdf07000b6ca02c72353d9d26c
Karol Baraniecki <karol@baraniecki.eu>
2021-11-09 21:31:16 +0100

    config

---
+++ config.py
@@ -0,0 +1,12 @@
+SESSION_SECRET_KEY = 'CHANGEME'
+DATABASE_HOST = '127.0.0.1'
+DATABASE_USER = 'sw'
+DATABASE_DB = 'sw'
+DATABASE_PW = 'CHANGEME'
+BASE_URL = 'https://wyborysamorzadpwr.pl' # used in emails
+SMTP_FROM_NAME = 'System Wyborczy Samorządu Studenckiego PWr'
+SMTP_FROM_ADDR = 'informatyzacja@samorzad.pwr.edu.pl'
+SMTP_LOGIN = 'informatyzacja@samorzad.pwr.edu.pl'
+SMTP_PW = 'CHANGEME'
+SMTP_HOST = 'student.pwr.edu.pl'
+SMTP_PORT = 587
