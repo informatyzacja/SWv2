bf21024fecde416399c9237d3c1164508070ac11
Karol Baraniecki <karol@baraniecki.eu>
2022-04-12 16:29:16 +0200

    Fix database creation (again)

--- sw-database/resetdb.sh
+++ sw-database/resetdb.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
-set -e
+set -ex
 
 cd /opt/sw/sw-database
 
-sudo -u postgres psql -c "
-    \\set AUTOCOMMIT on
-    CREATE USER sw WITH PASSWORD 'pw';
-    CREATE DATABASE sw WITH OWNER = sw;" || true
-
-sudo -u postgres psql sw -c '
-    drop table polls;
-    drop table users;' || true
+sudo -u postgres psql -c "DROP DATABASE sw;" || true
+sudo -u postgres psql -c "CREATE USER sw WITH PASSWORD 'pw';" || true
+sudo -u postgres psql -c "CREATE DATABASE sw WITH OWNER = sw;"
 
 sudo -u postgres psql sw -c "$(cat schema.sql)"
-
-#sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON polls TO sw;'
-#sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON users TO sw;'
-#sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON polls_id_seq TO sw;'
-#sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON users_id_seq TO sw;'
 
 sudo -u postgres psql sw -c '
     GRANT ALL PRIVILEGES ON polls TO sw;
