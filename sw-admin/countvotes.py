f31be03e9437d0a26cb8b051d381c6f61820fe64
DocentSzachista <damian.raczkowski.dr@gmail.com>
2022-07-19 10:07:55 +0200

    add check if option is in range of 0-99

--- sw-admin/countvotes.py
+++ sw-admin/countvotes.py
@@ -7,24 +7,26 @@ from collections import defaultdict
 
 def count_votes( poll_id, max_votes_allowed: int = -1):
 
-    regex = r"option_[0-9][0-9]*"
+    regex = r"^(option_([0-9][0-9]*))"
     pattern = re.compile(regex)
-    search_directory = f"/opt/sw/poll/{poll_id}/results/" # TODO: 
+    search_directory = f"/opt/sw/poll/{poll_id}/results/" 
 
     votes = defaultdict(int)
     for vote in os.listdir(search_directory): 
         vote_dir = os.path.join(search_directory, vote)
         with open(vote_dir, "r") as file :
-
-            file_contents = file.read()
-            marked_options = pattern.findall(file_contents)
-
-            if max_votes_allowed >= len(marked_options): 
-                temp_dict = dict()
-                for option in marked_options:
-                    temp_dict[option] = 1
-                for key in temp_dict:
-                    votes[key] += temp_dict[key]
+           
+           file_contents = file.read()
+           temp  = file_contents.replace("\n", "").split("&")
+           marked_options = dict()
+           for element in temp:
+               marked_option = pattern.match(element)
+               if int(marked_option.group(2)) < 100: 
+                   marked_options[marked_option.group(1)] = 1
+           
+           if max_votes_allowed >= len(marked_options):            
+               for key in marked_options:
+                   votes[key] += marked_options[key]
     return json.dumps(dict(votes))
 
 if __name__ == "__main__":
