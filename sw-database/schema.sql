87c46c0c5893ad2bc860b1f0d16a06f7a3261ea8
Jean Cz <3999172+janczarny@users.noreply.github.com>
2022-08-08 22:59:22 +0200

    Unified field type for - - planned_start_sending - closes_on_date

--- sw-database/schema.sql
+++ sw-database/schema.sql
@@ -57,7 +57,6 @@ create table polls(
     -- Czas kiedy głosowanie się kończy
     -- Format: "YYYY-MM-DD hh:mm"
     -- Na przykład: "2022-01-01 13:30"
-    -- Issue na zmianę typu na `timestamp with time zone`: https://github.com/informatyzacja-sspwr-projekty/SWv2/issues/10
     closes_on_date timestamptz not null,
 
     -- Czy sw-mailsender wysyła maile z `sending_out_to`?
@@ -83,5 +82,5 @@ create table polls(
 
     -- Jeżeli wartość nie jest NULL, o danej godzinie głosowanie zostanie rozpocząte
     -- (między innymi ustawiając `mailing_active` na true)
-    planned_start_sending timestamp with time zone
+    planned_start_sending timestamptz not null
 );
